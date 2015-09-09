fe683d8b99d3afce486e6e594750c5733ba15fe3
Oskar Eriksson <oskar.eriksson9@gmail.com>
2015-09-09 16:45:24 +0200

    First commit, node

---
+++ node.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Node struct {
+  Facing *Node
+  G, H int
+}
+func (n Node) F() int {
+  return n.G + n.H
+}
+  
+
+
+func main() {
+
+  start := Node{G: 2, H: 10}
+  node1 := Node{}
+  node1.Facing = &start
+  fmt.Printf("Node F is facing ", node1.Facing)
+}
