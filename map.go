dd766b95a63ed43328d0dbc235f6d377e79ece2f
Oskar Eriksson <oskar.eriksson9@gmail.com>
2015-09-10 21:14:27 +0200

    Now parses map tiles

--- map.go
+++ map.go
@@ -5,11 +5,24 @@ import "fmt"
 import "io/ioutil"
 import "log"
 
+// Two dimensional map, consisting of Tiles
+type Map map[int]map[int]*Tile
+type Tile struct {
+  X, Y int
+  M Map
+}
 
-type Map struct {
-  width, height int
-  tileWidth, tileHeight int
-  data []int
+func (m Map) addTile(t *Tile, x, y int) {
+  m[x][y] = t
+  t.X = x
+  t.Y = y
+  t.M = m
+}
+
+func calcTileCoords(index, mapWidth int) (int, int) {
+  x := index / mapWidth
+  y := index - x * mapWidth
+  return x, y
 }
 
 func parseMapFile() string {
@@ -24,13 +37,19 @@ func parseMapFile() string {
     log.Fatal(err)
   }
 
-
-  children, _ := jsonParsed.Search("layers", "data").Children()
-  for _, child := range children {
-      fmt.Println(child.Data())
+  mapWidth := int(jsonParsed.Path("width").Data().(float64))
+  // Only first layer from Tiled data is used, rest ignored
+  firstMapLayer := jsonParsed.Search("layers", "data").Index(0)
+  tileTypes, _ := firstMapLayer.Children()
+  for index, tileType := range tileTypes {
+    x, y := calcTileCoords(index, mapWidth)
+      fmt.Println(tileType.Data(), x, y)
   }
   return "value"
 }
+
+func parseMapData() {}
+
 func main() {
 
   parsedMap := parseMapFile()
